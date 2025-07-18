8b9a430bcb3a97cee0f7d51c552ad11183a1be41
Sahil <paalsahil04@gmail.com>
2025-07-18 23:58:52 +0530

    context finished

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,19 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useContext } from 'react'
-import { CartContext, useCart } from '@/providers/CartProvider'
-import { FlatList } from 'react-native'
-import CartListItem from '@/components/CartListItem'
+import { View, Text } from "react-native";
+import React from "react";
+import { useContext } from "react";
+import { CartContext, useCart } from "@/providers/CartProvider";
+import { FlatList } from "react-native";
+import CartListItem from "@/components/CartListItem";
+import Button from "@/components/Button";
 
 const CartScreen = () => {
-    const {items} =useCart();
+  const { items,total } = useCart();
   return (
-    <View>
-   <FlatList data={items} renderItem={({item})=><CartListItem cartItem={item}/>}
-   contentContainerStyle={{padding:10,gap:10}}
-   />
+    <View style={{ padding: 10 }}>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        contentContainerStyle={{ padding: 10, gap: 10 }}
+      />
+      <Text style={{marginTop:20,fontSize:20,fontWeight:'500'}}> Total : {total}</Text>
+      <Button text="Checkout" />
     </View>
-  )
-}
+  );
+};
 
-export default CartScreen
+export default CartScreen;
--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -7,18 +7,28 @@ import { randomUUID } from "expo-crypto";
 type CartType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem["size"]) => void;
-  updateQuantity:(itemId:string,amount:-1|1)=>void
+  updateQuantity: (itemId: string, amount: -1 | 1) => void;
+  total: number;
 };
 
 export const CartContext = createContext<CartType>({
   items: [],
   addItem: () => {},
-  updateQuantity:()=>{}
+  updateQuantity: () => {},
+  total: 0,
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
   const [items, setItems] = useState<CartItem[]>([]);
   const addItem = (product: Product, size: CartItem["size"]) => {
+    const existingItem = items.find(
+      (item) => item.product && item.size == size
+    );
+
+    if (existingItem) {
+      updateQuantity(existingItem.id, 1);
+      return;
+    }
     const newCartItem: CartItem = {
       id: randomUUID(),
       product,
@@ -30,12 +40,20 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     setItems([newCartItem, ...items]);
   };
 
-    const updateQuantity=(itemId:string,amount:-1|1)=>{
-            const updatedItems=items.map(item=>item.id!=itemId?item:{...item,quantity:item.quantity+amount})
-            setItems(updatedItems)
-    }
+  const updateQuantity = (itemId: string, amount: -1 | 1) => {
+    setItems(
+      items
+        .map((item) =>
+          item.id != itemId
+            ? item
+            : { ...item, quantity: item.quantity + amount }
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
+  const total=items.reduce((sum,item)=>(sum+=item.product.price*item.quantity),0);
   return (
-    <CartContext.Provider value={{ items, addItem,updateQuantity }}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
       {children}
     </CartContext.Provider>
   );

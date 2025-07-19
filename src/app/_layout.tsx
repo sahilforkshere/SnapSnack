f9a75fb0b6f5d4b56be71391b23611f46b80f1ad
Sahil <paalsahil04@gmail.com>
2025-07-19 12:44:15 +0530

    session

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,25 +1,26 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-import { StatusBar } from 'expo-status-bar';
-import CartProvider from '@/providers/CartProvider';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { StatusBar } from "expo-status-bar";
+import CartProvider from "@/providers/CartProvider";
+import AuthProvider from "@/providers/AuthProvider";
 
-import { useColorScheme } from '../components/useColorScheme.web';
-
-
-
-
+import { useColorScheme } from "../components/useColorScheme.web";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -45,19 +46,19 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <CartProvider >
-      <Stack>
-         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(admin)" options={{ headerShown: false }} />
-        <Stack.Screen name="(user)" options={{ headerShown: false }} />
-        <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
-        <Stack.Screen name="(auth)" options={{ headerShown:false }} />
-      </Stack>
-        <StatusBar style='dark'/>
-      </CartProvider>
-     
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <AuthProvider>
+        <CartProvider>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+            <Stack.Screen name="(user)" options={{ headerShown: false }} />
+            <Stack.Screen name="cart" options={{ presentation: "modal" }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          </Stack>
+          <StatusBar style="dark" />
+        </CartProvider>
+      </AuthProvider>
     </ThemeProvider>
-   
-  )
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,32 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import Button from '../components/Button';
-import { Link } from 'expo-router';
+import { View, Text, ActivityIndicator } from "react-native";
+import React from "react";
+import Button from "../components/Button";
+import { Link, Redirect } from "expo-router";
+import { useAuth } from "@/providers/AuthProvider";
+import { supabase } from "@/lib/supabase";
 
 const index = () => {
+  const { session, loading } = useAuth();
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  if (!session) {
+    return <Redirect href={"/sign-in"} />;
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
-      <Link href={'/(user)'} asChild>
+    <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
+      <Link href={"/(user)"} asChild>
         <Button text="User" />
       </Link>
-      <Link href={'/(admin)'} asChild>
+      <Link href={"/(admin)"} asChild>
         <Button text="Admin" />
       </Link>
-      <Link href={'/sign-in'} asChild>
+      <Link href={"/sign-in"} asChild>
         <Button text="Sign-In" />
       </Link>
-
+      <Button onPress={() => supabase.auth.signOut()} text="Sign-Out" />
     </View>
   );
 };
